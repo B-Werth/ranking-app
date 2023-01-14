163fb82e9b1e96bbd68178fd458740785446fe43
B-Werth <bastian619werth@gmail.com>
2023-01-14 22:20:19 +0100

    prisma call

--- src/pages/lists.tsx
+++ src/pages/lists.tsx
@@ -38,6 +38,7 @@ const lists = [
 ];
 
 const Lists: NextPage = ({ posts }) => {
+  console.log(posts);
   return (
     <>
       <Head>
@@ -46,10 +47,10 @@ const Lists: NextPage = ({ posts }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className="bg-indigo-500 shadow">
-        <p>}</p>
+        <p></p>
         <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            Lists:{posts.id}
+            Lists:{posts[0].name}
           </h1>
         </div>
       </header>
@@ -89,7 +90,7 @@ const Lists: NextPage = ({ posts }) => {
 
 export async function getStaticProps() {
   const posts = await prisma.user.findMany();
-  console.log(posts);
+
   return {
     props: { posts },
   };
