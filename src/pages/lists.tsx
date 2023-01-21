a67b0463d3911e4f0b10acd13dda637890a2893e
B-Werth <bastian619werth@gmail.com>
2023-01-21 16:25:49 +0100

    dnymatic routes

--- src/pages/lists.tsx
+++ src/pages/lists.tsx
@@ -36,15 +36,14 @@ const Lists: NextPage = () => {
                       <div className="sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-1 lg:aspect-h-1 relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:h-64">
                         <img
                           src={Liste.listcontent?.image}
-                          alt={Liste.imageAlt}
                           className="h-full w-full object-cover object-center"
                         />
                       </div>
                       <h3 className="mt-6 text-sm text-gray-500">
-                        <a href={Liste.id}>
+                        <Link href={"/list/" + Liste.id}>
                           <span className="absolute inset-0" />
                           {Liste.name}
-                        </a>
+                        </Link>
                       </h3>
                     </div>
                   ))
